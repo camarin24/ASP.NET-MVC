7ddf212ae0e04f06a05f390c81ff231e511d75d7
Cristian Marin <Cristian Marin>
2016-08-09 19:45:52 -0500

    first commit

---
+++ ProyectoAdo/ProyectoAdo/Controllers/DepartmentsController.cs
@@ -0,0 +1,22 @@
+﻿using ProyectoAdo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoAdo.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private EmployeeContext employeeContext = new EmployeeContext();
+        // GET: Departments
+        public ActionResult Index()
+        {
+           
+            List<Department> departmentsModel = employeeContext.Departments.ToList();
+            return View(departmentsModel);
+        }
+
+    }
+}
---
+++ ProyectoAdo/ProyectoAdo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using ProyectoAdo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoAdo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private EmployeeContext employeeContext = new EmployeeContext();
+        public ActionResult Index(int id)
+        {
+            
+            List<Employee> employeeModel = employeeContext.Employees.Where(m => m.DepartmentId == id).ToList();
+            return View(employeeModel);
+        }
+
+        public ActionResult Detail(int id)
+        {
+
+            Employee employeeModel = employeeContext.Employees.Single(m => m.ID == id);
+            return View(employeeModel);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
